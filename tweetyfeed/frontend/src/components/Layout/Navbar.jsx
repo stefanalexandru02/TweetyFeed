34b4f37c72632f23873607185344878be9133f09
Stefan Alexandru <stefanalexandru02@outlook.com>
2020-06-07 00:16:01 +0300

    clear code

--- tweetyfeed/frontend/src/components/Layout/Navbar.jsx
+++ tweetyfeed/frontend/src/components/Layout/Navbar.jsx
@@ -19,21 +19,6 @@ export class Navbar extends Component {
 
 
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        {/* <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#">Disabled</a>
-                            </li>
-                        </ul> */}
-                        {/* <form class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" />
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form> */}
                         <ul className="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
                         <ul className="navbar-nav ">
                             <li className="nav-item">
